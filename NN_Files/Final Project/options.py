5d9a9f84671080416d71583af0e6e4b7cfa3ce4d
nnelson01 <70921841+nnelson01@users.noreply.github.com>
2020-12-18 21:51:26 -0700

    NN Finished Final

--- NN_Files/Final Project/options.py
+++ NN_Files/Final Project/options.py
@@ -31,7 +31,32 @@ def european_binomial_pricer(spot: float, strike: float, expiry: float, rate: fl
     return round(disc*Ct, 2)
 
 def american_binomial_pricer(spot: float, strike: float, expiry: float, rate: float, div: float, vol: float, num: int, payoff: Callable) -> float:
-    pass 
+
+    treenodes = num + 1
+    h = expiry / num
+    u = np.exp(((rate-div) * h) + vol * np.sqrt(h))
+    d = np.exp(((rate-div) * h) - vol * np.sqrt(h))
+    probup = (np.exp((rate - div) * h) - d) / (u-d)
+    probdown = 1.0 - probup
+    disc = np.exp(-rate * h)
+    
+    discPu = disc * probup
+    discPd = disc* probdown
+    
+    Ct = np.zeros(treenodes)
+    St = np.zeros(treenodes)
+    
+    for i in range(treenodes):
+        St[i] = spot * (u**(num - i)) * (d**i)
+        Ct[i] = payoff(St[i], strike)
+        
+    for i in range((num - 1), -1, -1):
+        for k in range(i + 1):
+            Ct[k] = discPu * Ct[k] + discPd * Ct[k+1]
+            St[k] = St[k] / u
+            Ct[k] = np.maximum(Ct[k], payoff(St[k], strike))
+    
+    return Ct[0]
 
 def black_scholes_call(spot: float, strike: float, expiry: float, rate: float, div: float, vol: float) -> float:
     d1 = (np.log(spot/strike) + (rate - div + 0.5 * vol * vol) * expiry) / (vol * np.sqrt(expiry))
@@ -52,6 +77,15 @@ def black_scholes_call_delta(spot: float, strike: float, tau: float, rate: float
 
 ## Simulations
 def binomial_path(spot: float, expiry: float, rate: float, div: float, vol: float, num: int) -> np.ndarray:
-    pass
+    h = expiry / num
+    u = np.exp((rate - div) * h + np.sqrt(h) * vol)
+    d = np.exp((rate - div) * h - np.sqrt(h) * vol)
+    pstar = (np.exp((rate - div) * h) - d) / (u - d) 
+    z = np.random.uniform(0,1,size=num)
+    path = np.empty(num)
+    path[0] = spot
+    for i in range(1, num):
+        if z[i] >= pstar: path[i] = u * path[i-1]
+        else: z[i] = path[i] = d * path[i-1]
 
-   
+    return path
